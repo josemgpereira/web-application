53f65a0628d4931782dbfa8ac3e0bcfe1f24d4c1
José Pereira <jose.m.g.pereira@hotmail.com>
2020-05-15 16:48:33 +0100

    add @Repository annotation

--- src/main/java/pt/jp/webapplication/repositories/UserRepository.java
+++ src/main/java/pt/jp/webapplication/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package pt.jp.webapplication.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pt.jp.webapplication.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
